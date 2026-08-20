c9b7d2b2b9572b15c8d2924851db1b0e997513b3
uberslaw <50467524+uberslaw@users.noreply.github.com>
2026-08-20 10:47:45 +1000

    Update from RepoSync (BNELT5CG5152D8R)

--- _publish/_elev-wrap-once.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-$log = "C:\ProgramData\Heimdall\logs\republish-api-elevated.log"
-New-Item -ItemType Directory -Force -Path (Split-Path $log) | Out-Null
-"STARTED $(Get-Date -Format o)" | Set-Content "C:\Heimdall\_publish\elev-run-marker.txt"
-& "C:\Heimdall\.tmp-dbcheck\elevated-redeploy.ps1"
-$code = $LASTEXITCODE
-"EXIT=$code $(Get-Date -Format o)" | Add-Content "C:\Heimdall\_publish\elev-run-marker.txt"
-exit $code
